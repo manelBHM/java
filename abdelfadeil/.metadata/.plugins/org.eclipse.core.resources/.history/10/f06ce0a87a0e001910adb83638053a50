d36ccb90100b19ae7da247e115a208cc15aa39ed
abdelfadeil <abdalfadeil@gmail.com>
2019-01-03 12:40:54 +0100

    petit modifs

---
+++ abdelfadeil/.metadata/.plugins/org.eclipse.core.resources/.history/10/f06ce0a87a0e001910adb83638053a50
@@ -0,0 +1,11 @@
+package com.projet3.inter;
+
+public class Test {
+
+	public static void main(String[] args) {
+		IOperation complex = new Complex();
+		IOperation reel = new Reel();
+		
+	}
+
+}
--- abdelfadeil/Application9/src/EssaiConstructeurs.java
+++ abdelfadeil/Application9/src/EssaiConstructeurs.java
@@ -2,10 +2,10 @@
 public class EssaiConstructeurs {
 
 	public static void main(String[] args) {
-		B b = new B(true, 5);
-		System.out.println(b);
-		//b = new B(false, 5);
-		//System.out.println(b);
+		classNo2 classNo2 = new classNo2(true, 5);
+		System.out.println(classNo2);
+		classNo2 = new classNo2(false, 5);
+		System.out.println(classNo2);
 	}
 
 }
