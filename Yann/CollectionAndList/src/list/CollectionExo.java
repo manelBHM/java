5edee9e3ebaa46c09fd0c75849547574fd6cfedb
yann <yannelogegrah@gmail.com>
2019-01-09 14:02:20 +0100

    JAVA

--- Yann/CollectionAndList/src/list/CollectionExo.java
+++ Yann/CollectionAndList/src/list/CollectionExo.java
@@ -12,6 +12,15 @@ public class CollectionExo {
 		Collections.sort(x);	 
 	}
 	
+	
+	public static void trieDecroissant(final List<String> x)
+	{
+		//Tri dans l'ordre décroissant
+		
+		Collections.sort(x, Collections.reverseOrder());
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -24,10 +33,17 @@ public class CollectionExo {
 		liste.add("Black");
 	
 		System.out.println("Liste initiale : " + liste);
+		System.out.println("");
 		
          trier(liste);
            
-         System.out.println("Liste trie : " + liste);
+         System.out.println("Liste trié : " + liste);
+         System.out.println("");
+         
+         
+         trieDecroissant(liste);
+         
+         System.out.println("Liste triée en ordre decroissant : " + liste);
 	}
 
 }
