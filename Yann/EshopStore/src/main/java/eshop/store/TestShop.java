dc122cf8fcb3c7375a280f28562b8369da978a20
yann <yannelogegrah@gmail.com>
2019-02-19 22:22:49 +0100

    Stagiaire_Spring_Boot

--- Yann/EshopStore/src/main/java/eshop/store/TestShop.java
+++ Yann/EshopStore/src/main/java/eshop/store/TestShop.java
@@ -1,23 +1,24 @@
 package eshop.store;
 
+
 public class TestShop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Customer c1 = new Customer("Stanley", "54 rue Beliard", 75018);
+		Customer c1 = new Customer("Stanley", "54 rue Beliard", 75021);
 		
 		Product f1 = new Frigo("Frigo", "Whirpool, 1m10, Blanc", 300);
 		Product m1 =  new MicroOnde("Micro Onde", "High One, Blanc", 70);
 		Product cf1 = new Cafetiere("Cafetière", "Senseo, 2 litres, Noir Blanc", 50);
 		Product t1 = new Tele("Télévision", "Samsung, 150 pouces, Ecran Plasma", 800);
 		
-		Delivery express  = new ExpressDelivery("Stanley", "54 rue Beliard", 75018);
-		Delivery relay = new RelayDelivery(12);
+		Delivery express  = new ExpressDelivery("Stanley", "54 rue Beliard", 75021);
+		Delivery relay = new RelayDelivery(48);
 		Delivery home = new HomeDelivery();
 		
 		
-		Bill bill1 = new Bill(c1, express);
+		Bill bill1 = new Bill(c1, home);
 		
 		bill1.addProducts(t1, 1);
 		bill1.addProducts(m1, 2);
@@ -25,7 +26,7 @@ public class TestShop {
 		
 		bill1.getProducts();
 		
-		bill1.calcuTotal(bill1);
+		bill1.calcuTotal();
 		System.out.println("----------------");
 		System.out.println(bill1.getDelivery());
 		System.out.println(bill1);
