87bfb46c33d351b1c7cab4e0910593883a800898
yaser <abbasyaser989@gmail.com>
2019-02-22 14:48:14 +0100

    hello

--- App10/src/Chat.java
+++
@@ -1,7 +0,0 @@
-
-public class Chat extends Animal{
-
-	void devine() {
-		System.out.println("Je suis un Chat");
-	}
-}
--- App11/src/IOperation.java
+++
@@ -1,8 +0,0 @@
-
-public  interface IOperation {
-	
-	public Object plus (Object c);
-	
-	public Object moins (Object c);
-
-}
